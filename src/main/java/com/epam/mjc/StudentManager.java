2a3434322122fafdb3d21cbd7b49cce86a1fcc72
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-24 21:26:19 +0400

    asdasd

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -4,10 +4,10 @@ package com.epam.mjc;
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
+//
+//  public Student find(long studentID) {
+//    return Student.getValueOf(studentID);
+//  }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
@@ -16,10 +16,10 @@ public class StudentManager {
 
     for (int i = 0; i < IDs.length; i++) {
       Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+//      System.out.println("Student name " + student.getName());
     }
   }
-  public void find(int studentID) {
+  public Student find(long studentID) {
     for (int i = 0;i < IDs.length;i++) {
       if (studentID == IDs[i]) {
         try {
@@ -30,6 +30,7 @@ public class StudentManager {
         }
       }
     }
+    return null;
   }
 }
 
